2ec11a593f4283df9dcb3a01007a20c2c58778c2
Sandor Balogh <sandor.balogh@dawsoncollege.qc.ca>
2024-09-25 15:43:00 -0400

    Update code for to take a maximum of 6 quizzes and labs instead of 5, also specify to be able to give floats for grade values

--- Grade_Calculator.py
+++ Grade_Calculator.py
@@ -1,22 +1,22 @@
-AmountOfLabs = int(input("Enter the number of labs completed: "))
+AmountOfLabs = int(input("\nEnter the number of labs completed: "))
 AmountOfQuizzes = int(input("Enter the number of quizzes completed: "))
 
-Assignment1 = int(input("Enter the grade for Assignment 1: "))
-Assignment2 = int(input("Enter the grade for Assignment 2: "))
-Assignment3 = int(input("Enter the grade for Assignment 3: "))
-Assignment4 = int(input("Enter the grade for Assignment 4: "))
+Assignment1 = float(input("Enter the grade for Assignment 1: "))
+Assignment2 = float(input("Enter the grade for Assignment 2: "))
+Assignment3 = float(input("Enter the grade for Assignment 3: "))
+Assignment4 = float(input("Enter the grade for Assignment 4: "))
 
-Midterm1 = int(input("Enter the grade for Midterm 1: "))
-Midterm2 = int(input("Enter the grade for Midterm 2: "))
+Midterm1 = float(input("Enter the grade for Midterm 1: "))
+Midterm2 = float(input("Enter the grade for Midterm 2: "))
 
-FinalExam = int(input("Enter grade for Final Exam: "))
+FinalExam = float(input("Enter grade for Final Exam: "))
 
-FinalMidtermPreparations = int(input("Enter grade for Midterms and Final Preparation: "))
+FinalMidtermPreparations = float(input("Enter grade for Midterms and Final Preparation: "))
 
-if AmountOfLabs > 5: AmountOfLabs = 5
-if AmountOfQuizzes > 5: AmountOfQuizzes = 5
+if AmountOfLabs > 6: AmountOfLabs = 6
+if AmountOfQuizzes > 6: AmountOfQuizzes = 6
 
-StudentGrade = (AmountOfLabs * 4) + (AmountOfQuizzes * 3) + (Assignment1 * 0.04) + (Assignment2 * 0.04) + (Assignment3 * 0.04) + (Assignment4 * 0.04) + (Midterm1 * 0.125) + (Midterm2 * 0.125) + (FinalExam * 0.18) + (FinalMidtermPreparations * 0.06)
+StudentGrade = (AmountOfLabs * 3.333) + (AmountOfQuizzes * 3) + (Assignment1 * 0.04) + (Assignment2 * 0.04) + (Assignment3 * 0.04) + (Assignment4 * 0.04) + (Midterm1 * 0.125) + (Midterm2 * 0.125) + (FinalExam * 0.18) + (FinalMidtermPreparations * 0.06)
 
 StudentGrade = str(round(StudentGrade, 2))
 
