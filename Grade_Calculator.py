0a0baf8ff5d4e76fff72a14075d3ad22e03fc38f
Sandor Balogh <sandor.balogh@dawsoncollege.qc.ca>
2024-09-27 08:35:04 -0400

    Fix quiz bug

--- Grade_Calculator.py
+++ Grade_Calculator.py
@@ -16,7 +16,7 @@ FinalMidtermPreparations = float(input("Enter grade for Midterms and Final Prepa
 if AmountOfLabs > 6: AmountOfLabs = 6
 if AmountOfQuizzes > 6: AmountOfQuizzes = 6
 
-StudentGrade = (AmountOfLabs * 3.333) + (AmountOfQuizzes * 3) + (Assignment1 * 0.04) + (Assignment2 * 0.04) + (Assignment3 * 0.04) + (Assignment4 * 0.04) + (Midterm1 * 0.125) + (Midterm2 * 0.125) + (FinalExam * 0.18) + (FinalMidtermPreparations * 0.06)
+StudentGrade = (AmountOfLabs * 3.333) + (AmountOfQuizzes * 2.5) + (Assignment1 * 0.04) + (Assignment2 * 0.04) + (Assignment3 * 0.04) + (Assignment4 * 0.04) + (Midterm1 * 0.125) + (Midterm2 * 0.125) + (FinalExam * 0.18) + (FinalMidtermPreparations * 0.06)
 
 StudentGrade = str(round(StudentGrade, 2))
 
